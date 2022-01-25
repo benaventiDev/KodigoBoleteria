5216a689d0fce8e8405cf9dd2de0128eea00450b
Marigiko <mark221@hotmail.es>
2022-01-25 13:48:16 -0300

    logger añadido

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,6 +7,9 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 
 @SuppressWarnings("ALL")
 public class App
@@ -15,15 +18,22 @@ public class App
         return (int) Math.round(Math.random() * (num + 1));
     }
 
+    public static Logger log(String text) {
+        BasicConfigurator.configure();
+        Logger log = Logger.getLogger(text);
+        return log;
+    }
+
     public static void main( String[] args )
     {
         DB database = new DB();
         database.inicializarDB();
 
         boolean continuar = true;
+        Logger log = log("Mensaje");
 
         ArrayList<String> informacion = new ArrayList<>();
-        System.out.println("Bienvenido a Cinecode, ¿que pelicula desea ver? ");
+        log.info("Bienvenido a Cinecode, ¿que pelicula desea ver? ");
 
         Scanner scan = new Scanner(System.in);
         
@@ -33,51 +43,51 @@ public class App
             sala.setNroAsientosOcupados(randomNum(sala.getNroAsientosTotales()));
 
             try {
-                System.out.println(cartelera.getCarteleraPeliculas());
+                log.info(cartelera.getCarteleraPeliculas());
 
                 String eleccion = scan.next().toLowerCase();
                 sala.setNroSala(randomNum(10));
 
-                System.out.println("Estos son las fechas disponibles para esa pelicula: ");
-                System.out.println(cartelera.filtroFecha(eleccion));
-                System.out.println("Seleccione la fecha a la que desea asistir: ");
+                log.info("Estos son las fechas disponibles para esa pelicula: ");
+                log.info(cartelera.filtroFecha(eleccion));
+                log.info("Seleccione la fecha a la que desea asistir: ");
 
                 int eleccionInt = scan.nextInt();
                 ArrayList<Object> horariosDisponibles = cartelera.hrDisponible(eleccion, (LocalDate) cartelera.filtroFecha(eleccion).get(eleccionInt - 1));
 
-                System.out.println(horariosDisponibles);
-                System.out.println("Seleccione el horario al que desea asistir");
+                log.info(horariosDisponibles);
+                log.info("Seleccione el horario al que desea asistir");
 
                 int eleccionInt2 = scan.nextInt();
                 Object horario = cartelera.hrDisponible(eleccion, (LocalDate) cartelera.filtroFecha(eleccion).get(eleccionInt - 1)).get(eleccionInt2 - 1);
 
-                System.out.println(horario);
+                log.info(horario);
 
                 LocalDateTime dateFunction = LocalDateTime.of((LocalDate) cartelera.filtroFecha(eleccion).get(eleccionInt - 1), (LocalTime) horario);
 
-                System.out.println("Ingrese la cantidad de boletos que desea: ");
+                log.info("Ingrese la cantidad de boletos que desea: ");
 
                 int cantidad = scan.nextInt();
 
-                System.out.println("Ingrese si es mayor de edad: ");
+                log.info("Ingrese si es mayor de edad: ");
 
                 boolean mayor = scan.nextBoolean();
                 Boletos ticket = new Boletos(cantidad, randomNum(100), mayor, dateFunction);
 
-                System.out.println("Este es su ticket");
+                log.info("Este es su ticket");
 
                 String data = "Precio " + ticket.factura(cantidad) + " Numero de asiento: " + sala.nroAsiento(sala.getNroAsientosOcupados()) + " Numero de sala: " + sala.getNroSala();
                 informacion.add(data);
 
-                System.out.println(informacion);
+                log.info(informacion);
 
                 sala.setNroAsientosOcupados(cantidad);
 
-                System.out.println("¿Desea realizar otra compra?");
+                log.info("¿Desea realizar otra compra?");
 
                 continuar = scan.nextBoolean();
             } catch (Exception e) {
-                System.out.println("Algo no anda bien: " + e);
+                log.warn("Algo no anda bien: " + e);
             }
         }
         Imprimir impresora = new Imprimir();
