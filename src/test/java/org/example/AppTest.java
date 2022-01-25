037cbfdd38097e34cdd772814a97bdcc4927d512
Marigiko <mark221@hotmail.es>
2022-01-25 13:06:38 -0300

    test unitario de Boletos

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,12 +1,23 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import clases.Boletos;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
+class BoletasTest {
+    @Test
+    void getTotal() {
+        Boletos Boletas = new Boletos(7, 1, true, "test");
+        assertEquals(1470.0, Boletas.factura(7));
+        // assertEquals(4, Calculator.add(2, 2));
+    }
+}
+
 public class AppTest 
 {
     /**
@@ -15,6 +26,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        BoletasTest test = new BoletasTest();
+        test.getTotal();
     }
 }
